1f558de0c86f673eea7ba670de9e9c209327a2b8
LackFos <LackFosisOnline@gmail.com>
2024-10-04 13:16:57 +0700

    Buat API CRUD category

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\CategoryController;
 
 Route::prefix('/suppliers')->group(function() {
     Route::prefix('/v1')->group(function() {
@@ -12,3 +13,11 @@ Route::prefix('/suppliers')->group(function() {
     });
 });
 
+Route::prefix('/categories')->group(function() {
+    Route::prefix('/v1')->group(function() {
+        Route::get("/", [CategoryController::class, "all"]); // GET /categories/v1
+        Route::post("/", [CategoryController::class, "create"]); // POST /categories/v1
+        Route::put("/{category}", [CategoryController::class, "update"]); // PUT /categories/v1/:id
+        Route::delete("/{category}", [CategoryController::class, "delete"]); // DELETE /categories/v1/:id
+    });
+});
